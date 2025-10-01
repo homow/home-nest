266d798dec655249c16ee8f65926d5868aefc398
homow <homowsh897@gmail.com>
2025-10-01 21:01:18 +0330

    update SvgDefs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "@/Layout/Header"
 import MainLayout from "@/Layout/MainLayout";
 import Footer from "@/Layout/Footer";
+import SvgDefs from "@/components/ui/SvgDefs.jsx";
 
 export default function App() {
     return (
         <>
+            <SvgDefs/>
             <Header/>
             <MainLayout/>
             <Footer/>
