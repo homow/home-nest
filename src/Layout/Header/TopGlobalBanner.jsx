375872b4a982d5d6388db123d428f085ee6993ce
homow <homowsh897@gmail.com>
2025-10-02 18:28:47 +0330

    update TopGlobalBanner

--- src/Layout/Header/TopGlobalBanner.jsx
+++ src/Layout/Header/TopGlobalBanner.jsx
@@ -1,7 +1,7 @@
 export default function TopGlobalBanner() {
     return (
         <div className={"hidden xx:block relative overflow-hidden pt-10 pb-5 text-xs border-grey-15 border-b xs:text-sm sm:text-base sm:py-3.5"}>
-            <span className={"absolute -z-10 inset-0 -left-237 sm:left-0 opacity-40"}>
+            <span className={"absolute -z-10 inset-0 -left-237 sm:left-0 opacity-20 dark:opacity-40"}>
                 <svg className={"w-full"}>
                     <use href="#waveField-icon"></use>
                 </svg>
@@ -12,7 +12,7 @@ export default function TopGlobalBanner() {
                     <p>✨ خانه های رویایی رو با آشیانه پیدا کنید</p>
                     <a target={"_blank"} href="https://homow.ir" className={"underline"}>اطلاعات بیشتر</a>
                 </div>
-                <span className={"absolute right-4 top-1/2 -translate-y-1/2 inline-block bg-white/10 p-1 rounded-full cursor-pointer"}>
+                <span className={"absolute right-4 top-1/2 -translate-y-1/2 inline-block bg-black/18 dark:bg-white/10 p-1 rounded-full cursor-pointer"}>
                     <svg className={"size-3.5 xs:size-4 sm:size-5"}>
                         <use href="#close-icon"></use>
                     </svg>
