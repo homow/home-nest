c591eba3be799cd5d83acc2cfc7f173c8a88f4f7
homow <homowsh897@gmail.com>
2025-10-05 17:52:24 +0330

    update logo

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function Logo({className}) {
     return (
         <Link className={`w-full inline-block hover:opacity-100 ${className}`} to={"/"}>
-            <img className={"w-full"} src="/static/logo.png" alt="Logo picture"/>
+            <img className={"w-full"} src="/static/logo.webp" alt="Logo picture"/>
         </Link>
     )
 }
