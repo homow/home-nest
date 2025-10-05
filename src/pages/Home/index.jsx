0f5a207b96d5916f798f5176dcb6a104fbc26df0
homow <homowsh897@gmail.com>
2025-10-05 16:44:41 +0330

    completed FooterCopyRightAndSocial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
 import HeroSection from "./HeroSection/HeroSection.jsx";
 import FeaturedProperty from "./FeaturedProperty/FeaturedProperty.jsx";
+import {useEffect} from "react";
 
 export default function Home() {
+    useEffect(() => {
+        document.title = "صفحه اصلی | آشیانه";
+    }, [])
+
     return (
         <>
             <HeroSection/>
