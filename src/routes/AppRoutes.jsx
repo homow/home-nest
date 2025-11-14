f83ba373fa36ac05993086be774a73f8a033b40c
homow <homowsh897@gmail.com>
2025-11-14 11:51:45 +0330

    refactor AppRoutes with createBrowserRouter

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,18 @@
-import {useRoutes} from "react-router-dom"
-import Home from "@/pages/Home"
+import {createBrowserRouter} from "react-router-dom";
+import App from "@/App";
+import Home from "@/pages/Home";
 
-export default function AppRoutes() {
-    return useRoutes([
-        {path: "/", element: <Home/>},
-        {path: "/properties", element: <h1>Properties</h1>},
-        {path: "/about", element: <h1>About</h1>},
-        {path: "/contact", element: <h1>Contact</h1>},
-    ])
-}
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {index: true, element: <Home/>},
+            {path: "/properties", element: <h1>Properties</h1>},
+            {path: "/about", element: <h1>About</h1>},
+            {path: "/contact", element: <h1>Contact</h1>},
+        ]
+    },
+]);
+
+export default routes;
