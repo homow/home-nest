45c248c00691b1407be730f517563336f45c6984
homow <homowsh897@gmail.com>
2025-11-14 12:41:24 +0330

    add 404 page to routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,25 @@
+import {lazy} from "react";
 import {createBrowserRouter} from "react-router-dom";
-import App from "@/App";
-import Home from "@/pages/Home";
 import {BASE_PATH} from "@/config.js";
 
+const App = lazy(() => import("@/App"));
+const Home = lazy(() => import("@/pages/Home"));
+const NotFound = lazy(() => import("@/pages/NotFound"));
+
 const routes = createBrowserRouter(
-    [{
-        path: "/",
-        element: <App/>,
-        children: [
-            {index: true, element: <Home/>},
-            {path: "/properties", element: <h1>Properties</h1>},
-            {path: "/about", element: <h1>About</h1>},
-            {path: "/contact", element: <h1>Contact</h1>},
-        ]
-    }],
+    [
+        {
+            path: "/",
+            element: <App/>,
+            children: [
+                {index: true, element: <Home/>},
+                {path: "properties", element: <h1>Properties</h1>},
+                {path: "about", element: <h1>About</h1>},
+                {path: "contact", element: <h1>Contact</h1>},
+            ]
+        },
+        {path: "*", element: <NotFound/>}
+    ],
     {
         basename: BASE_PATH
     }
