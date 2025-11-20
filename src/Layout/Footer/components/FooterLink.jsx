9f700807a8d692abd03c5b9952f20a6c197f935e
homow <homowsh897@gmail.com>
2025-11-20 06:00:48 +0330

    refactor icon with Icon

--- src/Layout/Footer/components/FooterLink.jsx
+++ src/Layout/Footer/components/FooterLink.jsx
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import Logo from "@ui/Logo";
+import Icon from "@ui/icons/Icon";
 
 function FooterLinkBox({linkData}) {
     return (
@@ -30,20 +31,18 @@ function FooterLinkInput() {
                 <label className="group flex flex-row items-center gap-2 border border-grey-15 rounded-xl px-2 transition-all duration-300 focus-within:border-violet-600">
 
                     <label htmlFor={"email"} className={"hidden"}></label>
-                        <input
-                            id="email"
-                            type="text"
-                            name="email"
-                            placeholder="ایمیل"
-                            autoComplete="email"
-                            className="flex-1 w-full outline-none pl-5 py-3 rounded-l-lg focus:border-violet-600 bg-transparent"
-                        />
-
-                    <span className="inline-block max-w-max">
-                        <svg className="size-6">
-                            <use href="#send-icon"></use>
-                        </svg>
-                    </span>
+                    <input
+                        id="email"
+                        type="text"
+                        name="email"
+                        placeholder="ایمیل"
+                        autoComplete="email"
+                        className="flex-1 w-full outline-none pl-5 py-3 rounded-l-lg focus:border-violet-600 bg-transparent"
+                    />
+                    <Icon
+                        parentClassName={"max-w-max"}
+                        icon={"send"}
+                    />
                 </label>
             </form>
         </div>
