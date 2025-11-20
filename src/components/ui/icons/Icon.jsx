f4c1545631b602bcd598019f80773c4a55e7c8ff
homow <homowsh897@gmail.com>
2025-11-20 06:17:57 +0330

    clean

--- src/components/ui/icons/Icon.jsx
+++ src/components/ui/icons/Icon.jsx
@@ -2,9 +2,19 @@ import {cn} from "@/lib/utils/ui-utils.js";
 
 export default function Icon({icon, className, parentClassName}) {
     return (
-        <span className={cn("inline-block", parentClassName)}>
-            <svg className={cn("size-6", className)}>
-                <use href={`#${icon}-icon`}></use>
+        <span
+            className={
+                cn("inline-block", parentClassName)
+            }
+        >
+            <svg
+                className={
+                    cn("size-6", className)
+                }
+            >
+                <use
+                    href={`#${icon}-icon`}
+                />
             </svg>
         </span>
     );
