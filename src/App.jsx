f14bb5c440db37235e68472cbd3e102d41f92eec
homow <homowsh897@gmail.com>
2025-11-20 06:01:02 +0330

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router-dom";
 import Header from "@/Layout/Header"
 import Footer from "@/Layout/Footer";
 import MainLayout from "@/Layout/MainLayout";
-import SvgDefs from "@/components/ui/SvgDefs";
+import SvgDefs from "@ui/icons/SvgDefs";
 
 export default function App() {
     return (
