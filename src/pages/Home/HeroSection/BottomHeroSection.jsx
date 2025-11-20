c5996046bebbb24ff7b2dc5f34fea67cf2668145
homow <homowsh897@gmail.com>
2025-11-20 06:04:34 +0330

    refactor icons with Icon

--- src/pages/Home/HeroSection/BottomHeroSection.jsx
+++ src/pages/Home/HeroSection/BottomHeroSection.jsx
@@ -1,16 +1,19 @@
+import Icon from "@ui/icons/Icon.jsx";
+
 function BottomHeroSectionBox({text, icon}) {
     return (
         <div className={"relative bg-grey-10 rounded-custom border border-grey-15 text-center flex flex-col items-center py-5 px-3.5 text-sm md:text-base md:py-6 lg:py-7"}>
-            <span className={"absolute right-4 top-4"}>
-                <svg className={"size-6 text-grey-30"}>
-                    <use href="#longArrow-icon"></use>
-                </svg>
-            </span>
-            <span>
-                <svg className={"size-15"}>
-                    <use href={`#${icon}`}></use>
-                </svg>
-            </span>
+            <div className={"absolute right-4 top-4"}>
+                <Icon
+                    className={"text-gray-30"}
+                    icon={"longArrow"}
+                />
+            </div>
+
+            <Icon
+                className={"size-15"}
+                icon={icon}
+            />
             <p>{text}</p>
         </div>
     )
@@ -19,10 +22,10 @@ function BottomHeroSectionBox({text, icon}) {
 export default function BottomHeroSection() {
     return (
         <div className={"container grid grid-cols-2 gap-2.5 border border-grey-15 rounded-xl p-2.5 shadow-custom-no-blur md:grid-cols-4 md:max-w-none"}>
-            <BottomHeroSectionBox text={"خونه رویایی‌تو پیدا کن"} icon={"purpleSun-icon"}/>
-            <BottomHeroSectionBox text={"ارزش خونه‌تو آزاد کن"} icon={"purpleBuilding-icon"}/>
-            <BottomHeroSectionBox text={"مدیریت خونه و ملک بدون دردسر"} icon={"purpleCircleCart-icon"}/>
-            <BottomHeroSectionBox text={"سرمایه‌گذاری هوشمند، تصمیم‌گیری حساب‌شده"} icon={"purpleHouse-icon"}/>
+            <BottomHeroSectionBox text={"خونه رویایی‌تو پیدا کن"} icon={"purpleSun"}/>
+            <BottomHeroSectionBox text={"ارزش خونه‌تو آزاد کن"} icon={"purpleBuilding"}/>
+            <BottomHeroSectionBox text={"مدیریت خونه و ملک بدون دردسر"} icon={"purpleCircleCart"}/>
+            <BottomHeroSectionBox text={"سرمایه‌گذاری هوشمند، تصمیم‌گیری حساب‌شده"} icon={"purpleHouse"}/>
         </div>
-    )
-}
+    );
+};
