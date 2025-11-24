e55452a53cca7e5deab5ffc1d992deb314a2b499
homow <homowsh897@gmail.com>
2025-11-24 17:42:58 +0330

    fix root element

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react'
-import ReactDOM from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import {RouterProvider} from "react-router-dom"
 import "@/styles/global.css"
 import AppRoutes from "./routes/AppRoutes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
         <RouterProvider router={AppRoutes}/>
     </StrictMode>
