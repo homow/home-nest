4bea2dcf8320a781dd83ae33b48a514edf208800
homow <homowsh897@gmail.com>
2025-11-25 16:22:15 +0330

    update imports after change utils

--- src/components/common/PropertyCard.jsx
+++ src/components/common/PropertyCard.jsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import Icon from "@ui/icons/Icon.jsx";
+import Icon from "@ui/icons/Icon.tsx";
 
 export default function PropertyCard({img, title, description, features, price, propertyLink, className}) {
     return (
