dc6d681b8bf455b74f317afe5c60f3bffb274660
homow <homowsh897@gmail.com>
2025-11-26 13:13:51 +0330

    update

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,5 +1,5 @@
 import type {JSX} from "react";
-import {Link} from "react-router-dom";
+import {Link} from "react-router";
 import logo from "@/assets/logo.webp";
 import {cn} from "@utils/ui-utils";
 
