b946c93b4586add1e7636183659b2dce18a418f9
homow <homowsh897@gmail.com>
2025-11-26 11:23:06 +0330

    Add TypeScript types

--- src/components/common/TopTitleSections.jsx
+++ src/components/common/TopTitleSections.jsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import Icon from "@ui/icons/Icon.tsx";
+import Icon from "@ui/icons/Icon";
 
 export default function TopTitleSections({title, text, link = null, linkText}) {
     return (
