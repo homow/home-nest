b4695856d9a8171607a54c8e883643ea1bf847bd
homow <homowsh897@gmail.com>
2025-11-26 11:48:08 +0330

    Add TypeScript types to NotFound page

--- src/Layout/Footer/index.tsx
+++ src/Layout/Footer/index.tsx
@@ -1,10 +1,13 @@
+import type {JSX} from "react";
 import FooterLink from "./components/FooterLink";
 import FooterTop from "./components/FooterTop";
 import FooterCopyrightAndSocial from "./components/FooterCopyrightAndSocial"
 
-export default function Footer() {
+export default function Footer(): JSX.Element {
     return (
-        <footer className="mt-18 space-y-12">
+        <footer
+            className="mt-18 space-y-12"
+        >
             <FooterTop/>
             <FooterLink/>
             <FooterCopyrightAndSocial/>
