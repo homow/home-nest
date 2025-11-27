7959d6429154690b9248094425aa503ec2c8d299
homow <homowsh897@gmail.com>
2025-11-27 19:27:10 +0330

    clean TopGlobalBanner

--- src/Layout/Header/components/TopGlobalBanner.tsx
+++ src/Layout/Header/components/TopGlobalBanner.tsx
@@ -14,23 +14,39 @@ export default function TopGlobalBanner(): ReactNode {
     if (!visible) return null;
 
     return (
-        <div id="TopGlobalBanner" className={"relative overflow-hidden pt-10 pb-5 border-grey-15 border-b md:py-3.5"}>
+        <div
+            id="TopGlobalBanner"
+            className={"relative overflow-hidden pt-10 pb-5 border-grey-15 border-b md:py-3.5"}
+        >
             <Icon
                 parentClassName={"absolute -z-10 inset-0 -left-237 sm:left-0 opacity-20 dark:opacity-40"}
                 className={"size-full"}
                 icon={"waveField"}
             />
 
-            <div className={"container relative text-sm sm:text-base"}>
-                <div className={"flex flex-row items-center justify-end md:justify-center gap-2 sm:gap-4"}>
-                    <p>✨ خانه‌ٔ رویایی‌تو با آشیانه بساز</p>
-                    <a target="_blank" href="https://homow.ir" className="underline-link">جزئیات</a>
+            <div
+                className={"container relative text-sm sm:text-base"}
+            >
+                <div
+                    className={"flex flex-row items-center justify-end md:justify-center gap-2 sm:gap-4"}
+                >
+                    <p>
+                        ✨ خانه‌ٔ رویایی‌تو با آشیانه بساز</p>
+                    <a
+                        rel={"noreferrer noopener"}
+                        target="_blank"
+                        href="https://homow.ir"
+                        className="underline-link"
+                    >
+                        جزئیات
+                    </a>
                 </div>
                 <span
                     onClick={() => {
                         setVisible(false)
                         localStorage.setItem("topGlobalBannerVisible", "false")
                     }}
+
                     className={"absolute right-4 top-1/2 -translate-y-1/2 bg-glass backdrop-blur-sm rounded-full cursor-pointer p-1"}>
                     <Icon
                         parentClassName={"inline"}
