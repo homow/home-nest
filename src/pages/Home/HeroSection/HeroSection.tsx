3370a87bde5c4e2e0feb24274f51f0c618a1eb1b
homow <homowsh897@gmail.com>
2025-11-27 18:49:46 +0330

    clean HeroSection

---
+++ src/pages/Home/HeroSection/HeroSection.tsx
@@ -0,0 +1,24 @@
+import RightHeroSection from "./RightHeroSection";
+import LeftHeroSection from "./LeftHeroSection";
+import BottomHeroSection from "./BottomHeroSection";
+
+export default function HeroSection() {
+    return (
+        <section
+            id="hero-section"
+            className={"space-y-4 2xl:space-y-0"}
+        >
+            <div
+                className={"space-y-20 xl:space-y-1 xl:flex flex-row items-center gap-10 2xl:-mt-10"}
+            >
+                <RightHeroSection
+                    className={"basis-1/2"}
+                />
+                <LeftHeroSection
+                    className={"basis-1/2"}
+                />
+            </div>
+            <BottomHeroSection/>
+        </section>
+    );
+};
