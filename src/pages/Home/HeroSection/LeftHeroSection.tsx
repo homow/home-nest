6e4ce65eb6ed57ba27bb8844696fdccd23a17325
homow <homowsh897@gmail.com>
2025-11-27 18:48:47 +0330

    clean LeftHeroSection

--- src/pages/Home/HeroSection/LeftHeroSection.tsx
+++ src/pages/Home/HeroSection/LeftHeroSection.tsx
@@ -1,5 +1,6 @@
 import type {JSX} from "react";
 import {Link} from "react-router";
+import {cn} from "@utils/ui-utils";
 
 type MainProps = {
     className?: string;
@@ -13,7 +14,11 @@ interface Props extends MainProps {
 
 function LeftHeroSectionState({title, text, className = ""}: Props): JSX.Element {
     return (
-        <div className={`bg-grey-10 border border-grey-15 rounded-lg p-4 text-center sm:max-w-60 sm:text-start ${className}`}>
+        <div
+            className={
+                cn("bg-grey-10 border border-grey-15 rounded-lg p-4 text-center sm:max-w-60 sm:text-start", className)
+            }
+        >
             <strong className={"text-2xl xl:text-3xl"}>{title}</strong>
             <p className={"text-grey-60 text-sm xl:text-base md:mt-1"}>{text}</p>
         </div>
@@ -22,17 +27,37 @@ function LeftHeroSectionState({title, text, className = ""}: Props): JSX.Element
 
 function LeftHeroSectionInfo(): JSX.Element {
     return (
-        <div className={"mb-10 xl:mb-15"}>
-            <h1 className={"font-medium max-w-10/12 xs:max-w-8/12 sm:max-w-6/12 md:max-w-6/12 xl:max-w-125"}>
+        <div
+            className={"mb-10 xl:mb-15"}
+        >
+            <h1
+                className={"font-medium max-w-10/12 xs:max-w-8/12 sm:max-w-6/12 md:max-w-6/12 xl:max-w-125"}
+            >
                 با آشیانه، رویاهات رو سریع تر پیدا کن
             </h1>
-            <p className={"text-sm text-grey-60 mt-4 mb-10 sm:max-w-8/12 md:text-base md:max-w-7/12 lg:max-w-5/12 xl:max-w-120 xl:mb-14 xl:mt-6"}>
+            <p
+                className={
+                    "text-sm text-grey-60 mt-4 mb-10 sm:max-w-8/12 md:text-base md:max-w-7/12 lg:max-w-5/12 xl:max-w-120 xl:mb-14 xl:mt-6"
+                }
+            >
                 سفرِ پیدا کردن خونه رویایی‌ت از همین‌جا شروع میشه.
                 آگهی‌هامون رو بگرد، اون خونه‌ای که تو رویات دیدی رو پیدا کن.
             </p>
-            <div className={"space-y-4 sm:flex flex-row items-center sm:space-y-0 gap-4"}>
-                <Link className={"primary-btn block text-center"} to="/properties">دیدن خانه ها</Link>
-                <a className={"secondary-btn block text-center"} target={"_blank"} href="https://card.homow.ir">اطلاعات بیشتر</a>
+            <div
+                className={"space-y-4 sm:flex flex-row items-center sm:space-y-0 gap-4"}
+            >
+                <Link
+                    className={"primary-btn block text-center"}
+                    to="/properties"
+                >
+                    دیدن خانه ها
+                </Link>
+                <a
+                    className={"secondary-btn block text-center"}
+                    target={"_blank"}
+                    href="https://card.homow.ir">
+                    اطلاعات بیشتر
+                </a>
             </div>
         </div>
     )
@@ -46,10 +71,22 @@ export default function LeftHeroSection({className}: MainProps): JSX.Element {
 
             {/* state box hero section */}
             <div className={"grid grid-cols-2 gap-2 sm:flex xl:gap-5"}>
-                <LeftHeroSectionState className={"w-full"} title={"+200"} text={"مشتری های راضی"}/>
-                <LeftHeroSectionState className={"w-full"} title={"10هزار"} text={"خانه برای مشتری ها"}/>
-                <LeftHeroSectionState title={"+16"} text={"سال سابقه"} className={"col-span-2 sm:col-span-1 w-full"}/>
+                <LeftHeroSectionState
+                    className={"w-full"}
+                    title={"+200"}
+                    text={"مشتری های راضی"}
+                />
+                <LeftHeroSectionState
+                    className={"w-full"}
+                    title={"10هزار"}
+                    text={"خانه برای مشتری ها"}
+                />
+                <LeftHeroSectionState
+                    title={"+16"}
+                    text={"سال سابقه"}
+                    className={"col-span-2 sm:col-span-1 w-full"}
+                />
             </div>
         </div>
-    )
-}
+    );
+};
