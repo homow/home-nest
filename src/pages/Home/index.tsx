a1b6d823f8e397e87934e06f9abd31fb77c03824
homow <homowsh897@gmail.com>
2025-11-28 21:31:02 +0330

    config lazy scroll

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,18 @@
-import type {JSX} from "react";
 import useDocumentTitle from "@/hooks/useDocumentTitle";
 import HeroSection from "./HeroSection/HeroSection";
 import FeaturedProperty from "./FeaturedProperty/FeaturedProperty";
+import LazyScroll from "@ui/suspense/LazyScroll";
 
-export default function Home(): JSX.Element {
+export default function Home() {
     useDocumentTitle("صفحه اصلی | آشیانه");
 
     return (
         <>
             <HeroSection/>
-            <FeaturedProperty/>
+
+            <LazyScroll>
+                <FeaturedProperty/>
+            </LazyScroll>
         </>
     );
 };
